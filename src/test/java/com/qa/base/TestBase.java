865e332dc82f58c9a6df78e2174ce505e1e4ceee
Srinivas Naika <srinivas.naika@publicissapient.com>
2020-04-03 07:37:06 +0530

    updated first testcase

--- src/test/java/com/qa/base/TestBase.java
+++ src/test/java/com/qa/base/TestBase.java
@@ -1,6 +1,16 @@
 package com.qa.base;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 
 public class TestBase {
 	
@@ -21,6 +31,31 @@ public class TestBase {
         return extension;
  
     }
+	
+	public List<Map<String, String>> ReadDataFromCSVFile(String filepath) throws IOException, CsvException {
+
+		List<Map<String, String>> CSVData = new ArrayList<Map<String, String>>();
+		File file = new File(filepath);
+		FileReader reader = new FileReader(file);
+		CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+		List<String[]> alldata = csvReader.readAll();
+		String[] allKeys = alldata.get(0);
+
+		for (int i = 1; i < alldata.size(); i++) {
+
+			Map<String, String> map = new HashMap<String, String>();
+			String[] EachRow = alldata.get(i);
+
+			for (int j = 0; j < EachRow.length; j++) {
+				String key = allKeys[j].toLowerCase();
+				String value = EachRow[j].isEmpty() ? "Not Exist in CSV" : EachRow[j];
+
+				map.put(key, value);
+			}
+			CSVData.add(map);
+		}
+		return CSVData;
+	}
  
 
 }
--- src/test/java/com/qa/stepdefn/CommitFileStepDefn.java
+++ src/test/java/com/qa/stepdefn/CommitFileStepDefn.java
@@ -1,6 +1,8 @@
 package com.qa.stepdefn;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 import com.qa.base.TestBase;
 
@@ -13,16 +15,20 @@ public class CommitFileStepDefn extends TestBase {
 	
 	String filename;
 	boolean flag = false;
+	boolean flagnew;
+	
+	TestBase data;
+	List<Map<String, String>> CSVData;
 	
 	
 	@Given("^User is having the File \"([^\"]*)\" in drop location$")
 	public void user_is_having_the_File_in_drop_location(String arg1) throws Throwable {
 		
-		filename  = "C:\\Users\\srinaika\\git\\automonew\\src\\test\\resources\\" + arg1;
+		filename  = System.getProperty("user.dir") + "\\src\\test\\resources\\" + arg1;
 		System.out.println(filename);
 		
-	    
-	}
+	}    
+	
 
 	@Then("^verify file format$")
 	public void verify_file_format() throws Throwable {
@@ -47,38 +53,80 @@ public class CommitFileStepDefn extends TestBase {
 	
 	@Then("^Verify File Header Record Date$")
 	public void verify_File_Header_Record_Date() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
+		data = new TestBase();
+
+		CSVData = data.ReadDataFromCSVFile(filename);
+		
+		System.out.println(CSVData);
+		
+		Map<String, String> firstRow = CSVData.get(0);
+		
+		System.out.println(firstRow);
+		
+		if(firstRow.containsKey("add"))
+		{ 
+			flag =true;
+		}
+		System.out.println(flag);
+		
 	}
 
 	@Then("^Verify File Header Record Number of Records$")
 	public void verify_File_Header_Record_Number_of_Records() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		System.out.println("Number of Records in the file is "+CSVData.size());
+	    
 	}
 
 	@Then("^Verify File Header Record Function$")
 	public void verify_File_Header_Record_Function() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		
+	  
 	}
 
 	@Then("^Verify File Header Record ALS Bank Number$")
 	public void verify_File_Header_Record_ALS_Bank_Number() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		
+	   
 	}
 
 	@Then("^Verify File Header Record ALS Account Number$")
 	public void verify_File_Header_Record_ALS_Account_Number() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		for (int i =0; i<CSVData.size(); i++)
+		{
+			
+			String AccNo = CSVData.get(i).get("accountnumber");				
+			System.out.println(AccNo);
+			
+			//Account Number should be of 10 digit
+			
+			if(AccNo.length()== 10)
+			{ 
+			System.out.println("Account Number is of 10 Character and it is Pass " +AccNo);
+			flagnew = true;
+			}
+			else
+			{
+				flagnew = false;
+				break;
+				
+			}
+			
+			
+			
+			Assert.assertTrue(flagnew);
+			
+		}	
+	  
 	}
 
 	@Then("^Verify File Header Record CLN Pool$")
 	public void verify_File_Header_Record_CLN_Pool() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	   
 	}
 
 
